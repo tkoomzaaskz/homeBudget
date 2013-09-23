79bce5fe14ebf42c709874e5d57a9e3ae942dc8a
Tomasz Ducin <tomasz.ducin@gmail.com>
2013-09-23 23:26:25 +0200

    added git-ignore script

---
+++ gitignore.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+git update-index --assume-unchanged project/config/databases.yml
+git update-index --assume-unchanged public_html/.htaccess
+git update-index --assume-unchanged public_html/.htpasswd
