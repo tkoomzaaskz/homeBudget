f45dbc7a17a9b77afb629ff6dbdcd263aa1c4d62
Tomasz Ducin <tomasz.ducin@gmail.com>
2014-01-06 20:37:19 +0100

    url query string constructing refactored

--- project/apps/backend/modules/chart/templates/categoryPieSuccess.php
+++ project/apps/backend/modules/chart/templates/categoryPieSuccess.php
@@ -3,11 +3,17 @@ Do: <?php echo $form['date_to']->render(array('onchange' => 'javascript:reload()
 Kto: <?php echo $form['created_by']->render(array('onchange' => 'javascript:reload();')); ?>
 Grupuj: <?php echo $form['sum_subcategories']->render(array('onchange' => 'javascript:reload();')); ?>
 <br />
-<?php stOfc::createChart(
+<?php
+  $params = array(
+    'mode' => 'calculate',
+    'chart[date_from]' => $form->getDefault('date_from'),
+    'chart[date_to]' => $form->getDefault('date_to'),
+    'chart[created_by]' => $form->getDefault('created_by'),
+    'chart[sum_subcategories]' => $form->getDefault('sum_subcategories'),
+    'format' => 'txt'
+  );
+  stOfc::createChart(
   sfConfig::get('app_chart_size_width'),
   sfConfig::get('app_chart_size_height'),
-  '@chart_category_pie?mode=calculate'
-  .'&chart[date_from]='.$form->getDefault('date_from')
-  .'&chart[date_to]='.$form->getDefault('date_to')
-  .'&chart[created_by]='.$form->getDefault('created_by')
-  .'&chart[sum_subcategories]='.$form->getDefault('sum_subcategories'), false); ?>
+  '@chart_category_pie?' . http_build_query($params), false);
+?>
--- project/apps/backend/modules/chart/templates/monthlyBalanceBarsSuccess.php
+++ project/apps/backend/modules/chart/templates/monthlyBalanceBarsSuccess.php
@@ -2,10 +2,16 @@
 <?php echo $form['date_to']->render(array('onchange' => 'javascript:reload();')); ?>
 <?php echo $form['created_by']->render(array('onchange' => 'javascript:reload();')); ?>
 <br />
-<?php stOfc::createChart(
+<?php
+  $params = array(
+    'mode' => 'calculate',
+    'chart[date_from]' => $form->getDefault('date_from'),
+    'chart[date_to]' => $form->getDefault('date_to'),
+    'chart[created_by]' => $form->getDefault('created_by'),
+    'format' => 'txt'
+  );
+  stOfc::createChart(
   sfConfig::get('app_chart_size_width'),
   sfConfig::get('app_chart_size_height'),
-  '@chart_monthly_balance_bars?mode=calculate'
-  .'&chart[date_from]='.$form->getDefault('date_from')
-  .'&chart[date_to]='.$form->getDefault('date_to')
-  .'&chart[created_by]='.$form->getDefault('created_by'), false); ?>
+  '@chart_monthly_balance_bars?' . http_build_query($params), false);
+?>
