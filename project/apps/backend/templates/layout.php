01534e4b2ae0bee45f02f253528e2d503afd1c2b
Tomasz Ducin <tomasz.ducin@gmail.com>
2015-10-15 20:32:14 +0200

    google tag manager hotfix

--- project/apps/backend/templates/layout.php
+++ project/apps/backend/templates/layout.php
@@ -9,6 +9,15 @@
     <?php include_javascripts() ?>
   </head>
   <body>
+<!-- Google Tag Manager -->
+<noscript><iframe src="//www.googletagmanager.com/ns.html?id=GTM-WS7SCN"
+height="0" width="0" style="display:none;visibility:hidden"></iframe></noscript>
+<script>(function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+'//www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+})(window,document,'script','dataLayer','GTM-WS7SCN');</script>
+<!-- End Google Tag Manager -->
     <?php include_component('sfAdminDash','header'); ?>
     <?php echo $sf_content ?>
     <?php include_partial('sfAdminDash/footer'); ?>
